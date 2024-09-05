10d62f429597a80f32c6074b275e5cf5373fa4af
Hensor1 <113185006+Hensor1@users.noreply.github.com>
2024-09-05 16:29:13 +0200

    Lagt til javakode for knappetrykk og fragmenter. Xml filer i layout må oppdateres

--- app/src/main/java/com/example/s375063s375045/MainActivity.java
+++ app/src/main/java/com/example/s375063s375045/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.s375063s375045;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startSpillKnapp, omSpilletKnapp, preferanserKnapp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        startSpillKnapp = (Button) findViewById(R.id.startSpillKnapp);
+        omSpilletKnapp = (Button) findViewById(R.id.omSpilletKnapp);
+        preferanserKnapp = (Button) findViewById(R.id.preferanserKnapp);
+
+        startSpillKnapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            loadFragment(new startSpill());
+            }
+        });
+        omSpilletKnapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            loadFragment(new omSpill());
+            }
+        });
+        preferanserKnapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            loadFragment(new preferanser());
+            }
+        });
     }
-}
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+    }
